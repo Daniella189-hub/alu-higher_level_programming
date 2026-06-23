e030ed907bee7115a934957aa622b122efa7d2e9
Daniella <m.nishimwe@alustudent.com>
2026-06-23 11:47:36 -0700

    created extraline

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("{}".format(''.join(chr(97 + i) for i in range(26) if i not in [4, 16])), end='')
+print("{}".format(''.join(chr(97 + i) for i in range(26)
+                         if i not in [4, 16])), end='')
