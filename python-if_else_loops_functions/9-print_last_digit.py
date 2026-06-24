e5b41db4fedd62768f20398d3edce826a43e7807
Daniella <m.nishimwe@alustudent.com>
2026-06-24 04:03:48 -0700

    placed whitespace in between operators

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number= str(number)
-    last_digit= int(str(number[-1]))
+    number = str(number)
+    last_digit = int(str(number[-1]))
     print(f"{last_digit}", end="")
     return last_digit
