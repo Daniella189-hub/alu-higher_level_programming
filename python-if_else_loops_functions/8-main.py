4d71b5e3063818922afe09f22dfb1795c65c1c3c
Daniella <m.nishimwe@alustudent.com>
2026-06-24 03:28:00 -0700

    from lowercase to uppercase

--- python-if_else_loops_functions/8-main.py
+++ python-if_else_loops_functions/8-main.py
@@ -3,4 +3,8 @@ uppercase = __import__('8-uppercase').uppercase
 
 print(f'{uppercase("best")}')
 print(f'{uppercase("Best School 98 Battery street")}')
-
+print(f'{uppercase("z")}')
+print(f'{uppercase("holberton")}')
+print(f'{uppercase("Holberton School")}')
+print(f'{uppercase("Holberton School, 98 battery street")}')
+print(f'{uppercase("")}')
