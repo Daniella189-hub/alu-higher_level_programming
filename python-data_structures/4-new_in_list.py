4a5a0a276c9a8f631e08ea3aa2a26cceda892257
Daniella <m.nishimwe@alustudent.com>
2026-07-04 13:20:22 -0700

    made some changes

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
     area = my_list[:]
+    if idx < 0 or idx >= len(area):
+        return area
     area[idx] = element
-    if idx < 0 or idx >= len(my_list):
-        return area
-    else:
-        return area
+    return area
