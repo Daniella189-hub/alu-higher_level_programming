3050da73847d115b7ae44624bae5a531d3526c66
Daniella <m.nishimwe@alustudent.com>
2026-07-04 14:39:20 -0700

    matrix all integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         line = ""
         for i in range(len(row)):
             if i == len(row) - 1:
-                line += "{}".format(row[i])
+                line += "{:d}".format(row[i])
             else:
-                line += "{} ".format(row[i])
+                line += "{:d} ".format(row[i])
         print(line)
