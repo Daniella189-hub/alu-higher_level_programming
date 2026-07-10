289027f7368ec8d5c09fab48f72ec05ec2327a65
Daniella <m.nishimwe@alustudent.com>
2026-07-10 13:46:06 -0700

    made somee changes

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
     try:
-        raise Exception
-    except Exception:
-        print("Exception raised")
+        raise Exception("Exception raised")
+    except Exception as the_list:
+        print(the_list)
