04710dabd6b6e9843fb65741fc34b598ed99a9cd
Daniella189-hub <m.nishimwe@alustudent.com>
2026-07-29 17:17:12 +0200

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""This module supplies one function, print_square(size)."""
+"""This module supplies one function, text_indentation(text)."""
 
 
-def print_square(size):
-    """Prints a square with the character #.
+def text_indentation(text):
+    """Prints a text with 2 new lines after each '.', '?', and ':'.
 
     Args:
-        size: The side length of the square.
+        text: The string text to format and print.
 
     Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is less than 0.
+        TypeError: If text is not a string.
     """
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+    if type(text) is not str:
+        raise TypeError("text must be a string")
 
-    for _ in range(size):
-        print("#" * size)
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] in [".", "?", ":"]:
+            print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
