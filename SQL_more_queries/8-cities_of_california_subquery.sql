a5acb424a03033246ab9d49e891c0364fea9d344
Daniella <m.nishimwe@alustudent.com>
2026-07-29 04:57:03 -0700

    this is the right output

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
--- Lists all the cities of california.-- Lists all the cities of california.
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "california") ORDER BY id;
+-- Lists all the cities of california.
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "california") ORDER BY id;
